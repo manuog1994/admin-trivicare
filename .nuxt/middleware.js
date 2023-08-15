92dbe3557341d18d970b9a3216d087beea275f90
manuog1994 <manuelortegagaliano@outlook.com>
2023-08-15 14:44:39 +0200

    Actualizado .gitignore y desrastreados archivos innecesarios

--- .nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['admin'] = require('../middleware/admin.js')
-middleware['admin'] = middleware['admin'].default || middleware['admin']
-
-export default middleware
